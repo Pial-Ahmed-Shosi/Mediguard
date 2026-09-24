71ddd8118f3e8c5cf416e82504eea12ba13b1881
Pial Ahmed Shosi <23201162@uap-bd.edu>
2026-09-25 02:16:54 +0600

    Delete MediGuard directory

--- MediGuard/MediGuard/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MediGuard.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
